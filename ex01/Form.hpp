29f2319839a1532fc70772bf7e85afd74226e62d
julcalde <julcalde@student.42heilbronn.de>
2025-09-12 14:43:08 +0200

    added Form.cpp and Form.hpp. Break.

---
+++ ex01/Form.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 14:41:43 by julcalde          #+#    #+#             */
+/*   Updated: 2025/09/12 14:42:29 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifnder FORM_HPP
+# define FORM_HPP
+
+# include "Bureaucrat.hpp"
+# include <iostream>
+# include <string>
+# include <exception>
+
+
+
+#endif
