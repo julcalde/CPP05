2acc0023d68d58177696f7a54931c1ea0d77c3e0
julcalde <julcalde@student.42heilbronn.de>
2025-09-12 13:24:51 +0200

    Copied Makefile from previous cpp modules and readapted it. Defined structure in Bureaucrat.hpp.

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/12 13:16:55 by julcalde          #+#    #+#             */
+/*   Updated: 2025/09/12 13:22:28 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat
+{
+	private:
+		const std::string name;
+		int grade;
+
+	public:
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		const std::string& getName() const;
+		int getGrade() const;
+
+		void incrementGrade();
+		void decrementGrade();
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
